e87a4a6a8d9e3f95733033a368b858b23faed006
electrolyteJ <hawks.jamesf@gmail.com>
2023-12-10 12:09:13 +0800

    first commit

---
+++ android/composite-builds/startup/startup-api/src/main/java/com/jamesfchen/startup/AppDelegate.java
@@ -0,0 +1,14 @@
+package com.jamesfchen.startup;
+
+import android.content.Context;
+
+public class AppDelegate {
+    public void attachBaseContext(Context base) {
+        //代码stub
+
+    }
+    public void onCreate() {
+        //代码stub
+
+    }
+}
