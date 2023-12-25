ade70fb7677a8e869b5a556d1ba45f36ed7a9c3f
electrolyteJ <hawks.jamesf@gmail.com>
2023-12-25 22:49:33 +0800

    n

---
+++ android/apk/vi-optimizer-duplicated-files/src/main/java/com/electrolytej/vi/MalformedSymbolListException.kt
@@ -0,0 +1,12 @@
+package com.electrolytej.vi
+
+/**
+ * Copyright ® $ 2023
+ * All right reserved.
+ *
+ * @author: electrolyteJ
+ * @since: Dec/25/2023  Mon
+ */
+import java.lang.Exception
+
+class MalformedSymbolListException(msg: String?) : Exception(msg)
